a10047adaf2f62b78a43f18fb035857f70c28d56
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-05-17 10:27:41 +0000

    Add intro message and first user choice actions

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import pyfiglet
 
+# Setup the connection and authorizations between application and google sheets
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,10 +14,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('BookWormz')
 
+ascii_banner = pyfiglet.figlet_format(">>> BOOK-WORMZ <<<")
+print(ascii_banner)
+
 # new_sheet = SHEET.add_worksheet(title="testing", rows="100", cols="20")
 
-test = SHEET.worksheet('kevin1234')
+# worksheet_objs = SHEET.worksheets()
+# worksheets_list = []
+# for worksheet in worksheet_objs:
+#     worksheets_list.append(worksheet.title)
+# print(worksheets_list)
 
-data = test.get_all_values()
+# test = SHEET.worksheet('kevin1234')
 
-print(data)
+# data = test.get_all_values()
+
+# print(data)
+
+print("WELCOME TO BOOKWORMZ! Add, manage and review your favourite books :)")
+
+while True:
+    user_input = input("Press 'L' to login, or 'R' to register:\n")
+    if(user_input == 'L'):
+        print("login function")
+        break
+    elif (user_input == 'R'):
+        print("register function")
+        break
+    else:
+        print("Invalid choice, please go again!")
+
